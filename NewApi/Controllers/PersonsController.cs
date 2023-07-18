6707f525577be9abb0cb357b4a3a4538ede7e29e
BURCU TOKOL <19120205055@ismu.edu.tr>
2023-07-19 01:27:24 +0300

    Add project files.

---
+++ NewApi/Controllers/PersonsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using NewApi.Models;
+using NewApi.Operation;
+
+namespace NewApi.Controllers
+{
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+
+        [HttpGet]
+        public List<Person> Person()
+        {
+            return Persons.GetPersons();
+        }
+        [HttpGet("{id}")]
+        public Person GetPersonById(int Id) {
+            return Persons.GetPersonById(Id);   
+        }
+        [HttpGet] 
+        public Person GetPersonByIdQuery([FromQuery] int Id) {
+            return Persons.GetPersonById(Id);
+        }
+    }
+}
---
+++ NewApi/Operation/Persons.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Identity;
+using NewApi.Models;
+using System.Collections.Generic;
+
+namespace NewApi.Operation
+{
+    public static class Persons
+    {
+        private static List<Models.Person> PersonList;
+
+        public static List<Models.Person> GetPersons()
+        {
+            PersonList = new Faker<Models.Person>().RuleFor(p => p.Id, f => f.IndexFaker)
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName()).Generate(20);   
+            return PersonList;
+            
+        }
+        public static Models.Person GetPersonById(int Id)
+        {
+            return PersonList.Find(p => p.Id == Id); 
+        }
+
+    }
+}

9417c33fd93d019637d405bec8c5672c42b7ebfd
BURCU TOKOL <19120205055@ismu.edu.tr>
2023-08-14 00:02:06 +0300

    init

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BookStoreWebApi.Controllers
 {
@@ -6,6 +7,32 @@ namespace BookStoreWebApi.Controllers
     [Route("[controller]s")]
     public class BookController : ControllerBase
     {
-       
+        private static List<Book> BookList = new List<Book>()
+       {
+           new Book
+           {
+               Id= 1,
+               Title = "Kürk Mantolu Madonna",
+               GenreId = 1, //roman
+               PageCount = 177,
+               PublishDate = new System.DateTime(1943,01,01),
+           },
+           new Book
+           {
+               Id= 2,
+               Title = "Ben, Robot",
+               GenreId = 2, //bilim kurgu
+               PageCount = 240,
+               PublishDate = new System.DateTime(1950,02,1),
+           },
+           new Book
+           {
+               Id= 3,
+               Title = "1984",
+               GenreId = 2, //roman
+               PageCount = 352,
+               PublishDate = new System.DateTime(1949,08,06),
+           }};
+
     }
 }
